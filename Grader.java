2b99d7dcdb39f6091d3921523cc48988b731b623
Drew <1adwong@seattleschools.org>
2025-11-03 09:10:47 -0800

    Create Grader.java

---
+++ Grader.java
@@ -0,0 +1,21 @@
+public class Grader {
+   public static int grade(String userName, String shoeColor) {
+   int score = 0;
+   String vowels = AEIOUaeiou;
+   char lastChar  = userName.charAt(userName.length()-1);
+   if (shoeColor == white) {
+   score = 0;
+   }
+   else if (vowels.IndexOf(username.substring()) <= 1) {
+   score ++ 50;
+   }
+   if ((lastChar < 105) || (shoeColor != white)) {
+   score ++ 3000;
+   }
+   return score;
+   }
+   public static void main(String[] args) {
+   Grade grade1 = new Grade();
+   Grade grade2 = new Grade(DrewW-Gif, gray)
+   }
+}
