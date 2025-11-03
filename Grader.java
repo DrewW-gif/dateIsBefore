adf06b65bdbea5796d8829b6cff41590ec8ed702
Drew <1adwong@seattleschools.org>
2025-11-03 09:21:54 -0800

    Update Grader.java

--- Grader.java
+++ Grader.java
@@ -1,21 +1,25 @@
 public class Grader {
    public static int grade(String userName, String shoeColor) {
    int score = 0;
-   String vowels = AEIOUaeiou;
+   String vowels = "AEIOUaeiou";
    char lastChar  = userName.charAt(userName.length()-1);
-   if (shoeColor == white) {
+   if (shoeColor == "white") {
    score = 0;
    }
-   else if (vowels.IndexOf(username.substring()) <= 1) {
-   score ++ 50;
+   else if (vowels.indexOf(userName.substring(0, 1)) <= 1) {
+   score += 50;
    }
-   if ((lastChar < 105) || (shoeColor != white)) {
-   score ++ 3000;
+   if ((lastChar < 105) || (shoeColor != "white")) {
+   score += 3000;
    }
    return score;
    }
    public static void main(String[] args) {
-   Grade grade1 = new Grade();
-   Grade grade2 = new Grade(DrewW-Gif, gray)
+   System.out.println(grade("EasyPeasy", "black"));
+   System.out.println(grade("APeace", "white"));
+   System.out.println(grade("WhirledPeas", "white"));
+   System.out.println(grade("Oscar", "red"));
+   System.out.println(grade("Amy", "white"));
+   System.out.println(grade("Ben", "blue"));
    }
 }
